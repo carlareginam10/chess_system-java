f73ec45491a4066cde5204376cdf6cd206d2602b
carla regina <carlareginam10@gmail.com>
2020-08-14 20:54:28 -0300

    BoardException and defensive programming

--- src/bordergame/Board.java
+++ src/bordergame/Board.java
@@ -8,7 +8,9 @@ public class Board {
 	
 	// o construtor só vai receber quantidade de linhas e colunas
 	public Board(int rows, int columns) {
-		super();
+		if(rows < 1 || columns < 1) {
+			throw new BoardException ("Error create board: there must be at last 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		//Piace será estanciado qua a quantidade de linhas e colunas informado
@@ -21,32 +23,51 @@ public class Board {
 	}
 
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
+	
 	public int getColumns() {
 		return columns;
 	}
 
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	} 
-	
 	public Piece piece(int rows, int columns) {
+		if(!positionExists(rows,columns)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[rows][columns];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists (position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position:"+position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >=0 && column < columns;
+	}
+	
+	public boolean positionExists (Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+		
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position)!= null;
+		
+	}
+	
 	
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,8 +34,10 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook (board, Color.BLACK), new Position(2,1));
+		board.placePiece(new Rook (board, Color.BLACK), new Position(0,4));
 		board.placePiece(new King (board, Color.WHITE), new Position(0,4));
+	
+	
 	}
 
 }
